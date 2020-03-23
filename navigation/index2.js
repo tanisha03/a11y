e3f0f5eacbf282203206204605ef00f3ee97de35
Tanisha <tanisha031199@gmail.com>
2020-03-24 00:34:22 +0530

    added legend

--- navigation/index2.js
+++ navigation/index2.js
@@ -3,10 +3,12 @@ document.addEventListener("keydown", function(e) {
   // handling for change of menu when right arrow key is clicked
   if (e.keyCode === 39) {
     if (l.classList.contains("submenu_item")) {
+      //retrive the parent menu_item
       l = l.parentElement.parentElement;
     }
     if (l.nextElementSibling !== null) {
       r = l.nextElementSibling;
+      //focus on the <a> of the next menu_item
       r.firstElementChild.focus();
     }
   }
@@ -14,10 +16,12 @@ document.addEventListener("keydown", function(e) {
   // handling for change of menu when right arrow key is clicked
   if (e.keyCode === 37) {
     if (l.classList.contains("submenu_item")) {
+      //retrive the parent menu_item
       l = l.parentElement.parentElement;
     }
     if (l.previousElementSibling !== null) {
       r = l.previousElementSibling;
+      //focus on the <a> of the previous menu_item
       r.firstElementChild.focus();
     }
   }
